187fa9aef565a30f581b0612ec87bcf1b67397a2
Kamicron <ludovic.chevroulet@outlook.fr>
2023-03-20 09:52:14 +0100

    initialisation nuxt + premier composant

---
+++ pages/blog/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>test</h1>
+    <Article
+    titre = "Tutoriel Nuxt.js"
+    contenu = "Ceci est un article"
+    />
+  </div>
+</template>
+
+<script>
+import Article from '~/components/Article';
+
+export default {
+  components: {
+    Article
+  }
+}
+</script>
